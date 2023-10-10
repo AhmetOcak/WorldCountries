9d129e66ec2f2597104cb35e64f35526d30e692a
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-10-10 21:58:32 +0300

    modify text function added

--- app/src/main/java/com/worldcountries/common/Helpers.kt
+++ app/src/main/java/com/worldcountries/common/Helpers.kt
@@ -2,6 +2,10 @@ package com.worldcountries.common
 
 import android.content.Context
 import android.content.res.Configuration
+import android.graphics.Typeface
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StyleSpan
 import com.worldcountries.R
 import java.io.IOException
 
@@ -17,3 +21,21 @@ suspend inline fun <T> apiCall(crossinline call: suspend () -> T): Response<T> {
 
 fun getGridSpan(context: Context) =
     if (context.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) 2 else 3
+
+fun modifyText(subTitle: String, text: String?): SpannableString {
+    val fullText = "$subTitle $text"
+
+    val startIndex = fullText.indexOf(subTitle)
+    val endIndex = startIndex + subTitle.length
+
+    val spannedString = SpannableString(fullText)
+
+    spannedString.setSpan(
+        StyleSpan(Typeface.BOLD),
+        startIndex,
+        endIndex,
+        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+
+    return spannedString
+}
