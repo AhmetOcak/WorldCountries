415773d0ece181cd90f4b839ff975f7723aa3dd9
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-10-12 21:34:28 +0300

    some fields removed from entity

--- app/src/main/java/com/worldcountries/data/local/room/db/FavoriteCountriesDataBase.kt
+++ app/src/main/java/com/worldcountries/data/local/room/db/FavoriteCountriesDataBase.kt
@@ -2,13 +2,10 @@ package com.worldcountries.data.local.room.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import com.worldcountries.data.local.room.MyTypeConverters
 import com.worldcountries.data.local.room.dao.FavoriteCountriesDao
 import com.worldcountries.model.favorite_country.FavoriteCountryEntity
 
 @Database(entities = [FavoriteCountryEntity::class], version = 1)
-@TypeConverters(MyTypeConverters::class)
 abstract class FavoriteCountriesDataBase : RoomDatabase() {
 
     abstract fun favoriteCountryDao() : FavoriteCountriesDao
--- app/src/main/java/com/worldcountries/model/favorite_country/FavoriteCountryEntity.kt
+++ app/src/main/java/com/worldcountries/model/favorite_country/FavoriteCountryEntity.kt
@@ -9,7 +9,7 @@ data class FavoriteCountryEntity(
 
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
-    val id: Int,
+    val id: Int = 0,
 
     @ColumnInfo(name = "common_name")
     val commonName: String,
@@ -20,11 +20,14 @@ data class FavoriteCountryEntity(
     @ColumnInfo(name = "capital")
     val capital: String,
 
+    @ColumnInfo(name = "population")
+    val population: String,
+
     @ColumnInfo(name = "region")
     val region: String,
 
-    @ColumnInfo(name = "population")
-    val population: String,
+    @ColumnInfo(name = "sub_region")
+    val subRegion: String,
 
     @ColumnInfo(name = "timezone")
     val timezone: String,
@@ -32,45 +35,18 @@ data class FavoriteCountryEntity(
     @ColumnInfo(name = "alt_spellings")
     val altSpellings: String,
 
+    @ColumnInfo(name = "landlocked")
+    val landlocked: Boolean,
+
     @ColumnInfo(name = "flag_description")
     val flagDescription: String,
 
-    @ColumnInfo(name = "introduction")
-    val introduction: String,
-
-    @ColumnInfo(name = "location")
-    val location: String,
-
-    @ColumnInfo(name = "map_ref")
-    val mapRef: String,
-
-    @ColumnInfo(name = "coastline")
-    val coastline: String,
-
-    @ColumnInfo(name = "climate")
-    val climate: String,
-
-    @ColumnInfo(name = "terrain")
-    val terrain: String,
-
-    @ColumnInfo(name = "pop_dist")
-    val popDist: String,
-
-    @ColumnInfo(name = "nationality")
-    val nationality: String,
+    @ColumnInfo(name = "map_url")
+    val mapUrl: String,
 
     @ColumnInfo(name = "flag_img_url")
     val flagImgUrl: String,
 
     @ColumnInfo(name = "coat_of_arms")
-    val coatOfArms: String,
-
-    @ColumnInfo(name = "ethnic_groups_chart")
-    val ethnicGroupsChart: List<ChartDataEntity>,
-
-    @ColumnInfo(name = "religion_chart")
-    val religionChart: List<ChartDataEntity>,
-
-    @ColumnInfo(name = "population_chart")
-    val populationChart: List<ChartDataEntity>
+    val coatOfArms: String
 )
