a5ebea2790daf924e3c2bd10c837a7133787c565
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-10-13 22:58:01 +0300

    unused fields removed

--- app/src/main/java/com/worldcountries/model/country/Country.kt
+++ app/src/main/java/com/worldcountries/model/country/Country.kt
@@ -7,13 +7,10 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Country(
     @SerializedName("name") val name: Name? = Name(),
-    @SerializedName("independent") val independent: Boolean? = null,
-    @SerializedName("currencies") val currencies: Currencies? = Currencies(),
     @SerializedName("capital") val capital: ArrayList<String> = arrayListOf(),
     @SerializedName("altSpellings") val altSpellings: ArrayList<String> = arrayListOf(),
     @SerializedName("region") val region: String? = null,
     @SerializedName("subregion") val subregion: String? = null,
-    @SerializedName("latlng") val latlng: ArrayList<Double> = arrayListOf(),
     @SerializedName("landlocked") val landlocked: Boolean? = null,
     @SerializedName("maps") val maps: Maps? = Maps(),
     @SerializedName("population") val population: Int? = null,
@@ -21,9 +18,5 @@ data class Country(
     @SerializedName("timezones") val timezones: ArrayList<String> = arrayListOf(),
     @SerializedName("continents") val continents: ArrayList<String> = arrayListOf(),
     @SerializedName("flags") val flags: Flags? = Flags(),
-    @SerializedName("coatOfArms") val coatOfArms: CoatOfArms? = CoatOfArms(),
-    @SerializedName("startOfWeek") val startOfWeek: String? = null,
-    @SerializedName("postalCode") val postalCode: PostalCode? = PostalCode(),
-    @SerializedName("borders") val borders: ArrayList<String> = arrayListOf(),
-    @SerializedName("area") val area: Double? = null
+    @SerializedName("coatOfArms") val coatOfArms: CoatOfArms? = CoatOfArms()
 ) : Parcelable
